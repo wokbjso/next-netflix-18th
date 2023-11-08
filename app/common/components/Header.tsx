17b0717ee3c195239196a8f5071143c9ff0b0557
김지원 <jibom0507@naver.com>
2023-11-09 03:45:59 +0900

    Chore:header 확인용 코드 수정

--- app/common/components/Header.tsx
+++ app/common/components/Header.tsx
@@ -4,7 +4,7 @@ import netflixlogo from "/public/netflix_logo.svg";
 
 export const Header = () => {
   return (
-    <div className="flex z-100 items-center justify-center justify-around mt-6 h-8rem w-37.5rem pr-6 header-text text-menu-main bg-background-main">
+    <div className="flex z-100 items-center justify-center justify-around mt-6 h-8rem w-37.5rem pr-6 header-text text-menu-main bg-transparent">
       <Image src={netflixlogo} width="57" height="57" alt="logo" />
       <a className="hover:text-button-main cursor-pointer transition duration-300 ease-in-out">
         TV Shows
