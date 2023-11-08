3022e782045a484d139a3125dc0a051b4c6c3b78
김지원 <jibom0507@naver.com>
2023-11-09 01:34:02 +0900

    Feat: Header 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./common/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-xl h-[3rem] w-[3rem] bg-background-main text-text-main font-system-heading1">
-      jhel
+    <div>
+      <Header />
     </div>
   );
 }
