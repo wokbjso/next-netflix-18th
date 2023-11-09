f1919ab18cb8b40f7fcc6d4b9f1982c37ee01f6d
김지원 <jibom0507@naver.com>
2023-11-09 17:35:15 +0900

    Style: Icon 반응형 수정

--- app/common/components/Footer.tsx
+++ app/common/components/Footer.tsx
@@ -9,23 +9,23 @@ export const Footer = () => {
   return (
     <div className="fixed inset-x-0 bottom-0 flex footer-text items-center justify-center w-37.5rem h-4.8rem pt-2 pb-2 bg-footerBackground-main text-footerIcon-main bg-background-main">
       <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <BiHomeAlt2 style={{ fontSize: "24px" }} />
+        <BiHomeAlt2 style={{ fontSize: "2.4rem" }} />
         <div className="mt-px">Home</div>
       </div>
       <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <LuSearch style={{ fontSize: "24px" }} />
+        <LuSearch style={{ fontSize: "2.4rem" }} />
         <div className="mt-px">Search</div>
       </div>
       <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <MdOutlineVideoLibrary style={{ fontSize: "24px" }} />
+        <MdOutlineVideoLibrary style={{ fontSize: "2.4rem" }} />
         <div className="mt-px">Coming Soon</div>
       </div>
       <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <HiDownload style={{ fontSize: "24px" }} />
+        <HiDownload style={{ fontSize: "2.4rem" }} />
         <div className="mt-px">Downloads</div>
       </div>
       <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <GiHamburgerMenu style={{ fontSize: "24px" }} />
+        <GiHamburgerMenu style={{ fontSize: "2.4rem" }} />
         <div className="mt-px">More</div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Footer from "./common/components/Footer";
+
 export default function Home() {
   return (
-    <div className="text-xl h-[3rem] w-[3rem] bg-background-main text-text-main font-system-heading1">
-      jhel
+    <div>
+      <div className="text-xl h-[3rem] w-[3rem] bg-background-main text-text-main font-system-heading1">
+        jhel
+      </div>
+      <Footer />
     </div>
   );
 }
