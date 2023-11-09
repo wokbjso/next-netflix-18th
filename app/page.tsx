c6477f8adedd4ba3e277160c99664a282933da04
brian <90752841+wokbjso@users.noreply.github.com>
2023-11-09 21:36:12 +0900

    Feat:3.5초후 화면 메인페이지로 전환

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+import { useEffect } from "react";
 import LandingNetflix from "./components/LandingNetflix";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/main");
+    }, 3500);
+  });
   return (
     <div className="w-full h-full bg-background-main flex justify-center items-center">
       <LandingNetflix />
