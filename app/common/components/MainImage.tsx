8ecf25be4a000e42a18cc8cc7a969d651ee69903
김지원 <jibom0507@naver.com>
2023-11-10 04:40:46 +0900

    Feat: MainImage 컴포넌트

---
+++ app/common/components/MainImage.tsx
@@ -0,0 +1,13 @@
+export const MainImage = () => {
+  return (
+    <div>
+      <img
+        src="https://i.namu.wiki/i/xNrcHofDqsp_wkeZMoc4dNV9TwexwXT2Br46HFJ29udUIfmgiuEhtuGRBvOdQZjFLjOiFMCJAkPb3-2VC2orDg.webp"
+        alt="poster"
+        className="relative w-[37.5rem] h-[41.5rem]"
+      />
+    </div>
+  );
+};
+
+export default MainImage;
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -4,6 +4,7 @@ import { getPopularMovieResponse } from "./queries/dto/get-popular-movie";
 import Header from "../common/components/Header";
 import Footer from "../common/components/Footer";
 import PlayBar from "../common/components/PlayBar";
+import MainImage from "../common/components/MainImage";
 
 export default async function Main() {
   const sliderInfo = [
@@ -27,11 +28,7 @@ export default async function Main() {
   return (
     <div className="bg-background-main">
       <Header />
-      <img
-        src="https://i.namu.wiki/i/xNrcHofDqsp_wkeZMoc4dNV9TwexwXT2Br46HFJ29udUIfmgiuEhtuGRBvOdQZjFLjOiFMCJAkPb3-2VC2orDg.webp"
-        alt="poster"
-        className="w-[37.5rem] h-[41.5rem]"
-      />
+      <MainImage />
       <PlayBar />
       <Footer />
     </div>
