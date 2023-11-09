60e60cfb98433f5164b861513568398e7bafe862
brian <90752841+wokbjso@users.noreply.github.com>
2023-11-10 00:05:50 +0900

    Feat:api url 변수로 정리

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,3 +1,25 @@
+import CustomSlider from "./components/Slider/CustomSlider";
+import getMovies from "./queries/getMovies";
+import { getPopularMovieResponse } from "./queries/dto/get-popular-movie";
+
 export default async function Main() {
+  const sliderInfo = [
+    {
+      text: "Popular on Netflix",
+      url: "https://api.themoviedb.org/3/movie/popular",
+    },
+    {
+      text: "Now Playing",
+      url: "https://api.themoviedb.org/3/movie/now_playing",
+    },
+    {
+      text: "Top Rated",
+      url: "https://api.themoviedb.org/3/movie/top_rated",
+    },
+    {
+      text: "Upcoming Contents",
+      url: "https://api.themoviedb.org/3/movie/upcoming",
+    },
+  ];
   return <div>hello</div>;
 }
--- app/main/queries/getMovies.tsx
+++ app/main/queries/getMovies.tsx
@@ -1,5 +1,3 @@
-import { getPopularMovieResponse } from "./dto/get-popular-movie";
-
 export default async function getMovies(url: string) {
   const apiKey =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDJjYWU5NGI2Yzk1MWNiY2EyMmVjMTc5Y2JmZDM3ZSIsInN1YiI6IjYzZDhiNDYzM2RjMzEzMDA4MjMyZTkyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fMlQjoW-zgChmdjBWD4oB0mHbaJ4fRjmXL8XAGougHY";
