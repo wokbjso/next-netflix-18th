b05d2395a8470932c52dba91fa9f1d398e18342e
김지원 <jibom0507@naver.com>
2023-11-09 17:28:42 +0900

    Style:로고 반응형으로 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Footer from "./common/components/Footer";
+import Header from "./common/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-xl h-[3rem] w-[3rem] bg-background-main text-text-main font-system-heading1">
-      jhel
+    <div>
+      <Header />
+      <Footer />
     </div>
   );
 }
