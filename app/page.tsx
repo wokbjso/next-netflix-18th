6eb29757cbb21bce41910f13b72d9ac2bc5c8635
김지원 <jibom0507@naver.com>
2023-11-09 17:50:34 +0900

    Chore:page.tsx 복구

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Footer from "./common/components/Footer";
-
 export default function Home() {
   return (
-    <div>
-      <div className="text-xl h-[3rem] w-[3rem] bg-background-main text-text-main font-system-heading1">
-        jhel
-      </div>
-      <Footer />
+    <div className="text-xl h-[3rem] w-[3rem] bg-background-main text-text-main font-system-heading1">
+      jhel
     </div>
   );
 }
