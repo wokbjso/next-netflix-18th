f70f694607d78e5acd14aeda3d6803291223b788
김지원 <jibom0507@naver.com>
2023-11-10 09:04:31 +0900

    Style:넷플릭스 로고 변경

--- app/common/components/Header.tsx
+++ app/common/components/Header.tsx
@@ -1,13 +1,10 @@
 "use client";
-import Image from "next/image";
-import netflixlogo from "/public/netflix_logo.svg";
+import { SiNetflix } from "react-icons/si";
 
 export const Header = () => {
   return (
     <div className="fixed inset-x-0 top-0 flex z-50 items-center justify-around pt-6 h-8rem w-37.5rem pr-6 header-text text-menu-main bg-transparent">
-      <div className="h-14 w-14 relative">
-        <Image src={netflixlogo} alt="logo" />
-      </div>
+      <SiNetflix style={{ fontSize: "3.2rem", color: "#B1060F" }} />
       <a className="hover:text-button-main cursor-pointer transition duration-300 ease-in-out">
         TV Shows
       </a>
